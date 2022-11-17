3dc3cc881ffdb51455252ec203f6ed6085221637
erico <ecocaval@hotmail.com>
2022-11-17 01:45:46 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
         <div>
             <Navbar/>
             <Corpo/>
+            <FundoMobile/>
         </div>
     );
 }
--- src/components/Corpo.js
+++ src/components/Corpo.js
@@ -9,7 +9,6 @@ export default function Corpo() {
                 <Stories/>
                 <Posts/>
             </div>
-
             <Sidebar/>
         </div>
     );
