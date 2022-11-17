4876c323936dc0a5ae445b0097f82ae9a7229015
erico <ecocaval@hotmail.com>
2022-11-17 01:31:08 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+import Corpo from "./components/Corpo";
+
+export default function App() {
+    return (
+        <div>
+            <Navbar/>
+            <Corpo/>
+        </div>
+    );
+}
